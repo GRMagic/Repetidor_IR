32a07046c165778a9289a32659244a28819d2966
GRMagic <gustavo.rm90@gmail.com>
2016-01-20 17:17:32 -0200

    Create Repetidor_IR.h

---
+++ Repetidor_IR.h
@@ -0,0 +1,12 @@
+struct ComandoIR {
+  int tipo;
+  unsigned long valor;
+  int bits;
+};
+
+#define PINO_BUZZER 10
+#define PINO_SENSOR 2
+// FIXO PINO_LED_IR 3
+#define PINO_PILOTO 4
+#define PINO_GRAVAR 9
+
