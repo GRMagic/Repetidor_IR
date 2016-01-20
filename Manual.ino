e898b8f079aa933574e54af0e1387fee91267e5a
GRMagic <gustavo.rm90@gmail.com>
2016-01-20 17:16:16 -0200

    Create Manual.ino

---
+++ Manual.ino
@@ -0,0 +1,37 @@
+/*
+ * ***** Geral *****
+ * 
+ * Recomendado limpar a memória antes de iniciar a configuração do aparelho.
+ * 
+ * O aparelho recebe o sinal de um controle e reproduz outro sinal configurado.
+ * Esse outro sinal pode ser o mesmo, ou ser um sinal deferente.
+ * 
+ * ***** Limpeza de memória *****
+ * 
+ * Manter o botao precionado por 10 segundos,
+ * o aparelho irá fazer 1 bip ao iniciar a limpeza da memória,
+ * quando o reset terminar o aparelho fará 2 bips.
+ * 
+ * ***** Gravação ou alteração de comando *****
+ * 
+ * Precionar o botão rápidamente
+ * A aparelho irá fazer um bip
+ * Aperte 3x o botão do controle de entrada
+ * A aparelho irá fazer outro bip
+ * Aperte 3x o botão do controle de saída
+ * A aparelho irá fazer dois bip rápidos
+ * 
+ * Caso o aparelho emita um bip longo, houve algum erro.
+ * Os erros mais comuns são:
+ * -Protocolo Inválido! Atualmente apenas os protocolos NEC, SONY, RC5 e RC6 são aceitos,
+ * outros procolos não foram testados, por isso não ativei.
+ * -Falha na leitura da tecla! Tente novamente, não mantenha a tecla precionada, 
+ * apenas aperte e solte
+ * -Não há memória disponível! Tente limpar a memória e gravar novamente.
+ * 
+ * **********************************************************************
+ *                                           Repetidor de Controle IR
+ *                                           Gustavo Rossi Müller
+ *                                           gustavo.rm90@gmail.com
+ *  
+ */
